f0e17710e0aa90a6b81fbbf7f82d74f5d5e6993b
Arjun <heyarjun138@gmail.com>
2025-11-17 15:06:12 +0530

    Updated

--- terraform/cluster/eks/github-ips.tf
+++ terraform/cluster/eks/github-ips.tf
@@ -1,20 +1,16 @@
 # Fetch GitHub Actions Runner IP ranges 
-
 data "http" "github_ips" {
   url = "https://api.github.com/meta"
 
-  # Optional: forces Terraform to fetch fresh data each time
   request_headers = {
     Accept = "application/json"
   }
 }
 
 # Extract only IPv4 CIDRs used by runners
-
 locals {
-  # GitHub returns IPv4 + IPv6. EKS only accepts IPv4.
   github_actions_ips = [
-    for cidr in jsondecode(data.http.github_ips.body).actions :
+    for cidr in jsondecode(data.http.github_ips.response_body).actions :
     cidr if can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/", cidr))
   ]
 }
