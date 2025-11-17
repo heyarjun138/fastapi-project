216abc11509d55fb59cef3cd641ac62320a38add
Arjun <heyarjun138@gmail.com>
2025-11-17 14:16:28 +0530

    Updated s3 state key

--- terraform/cluster/eks/backend.tf
+++ terraform/cluster/eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-backend-bucket-capestone"
-    key          = "infra/eks/terraform.tfstate"
+    key          = "infra/cluster/eks/terraform.tfstate"
     region       = "us-east-1" #Must match the bucket region
     use_lockfile = true        #Enabling S3 State Locking
     encrypt      = true
