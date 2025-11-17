51fb93246e700a42b7ec737c4abf5a5a8fa58ac8
Arjun <heyarjun138@gmail.com>
2025-11-17 19:56:40 +0530

    updated eks tf file

--- terraform/cluster/eks/eks.tf
+++ terraform/cluster/eks/eks.tf
@@ -1,19 +1,12 @@
-#############################################
-# FINAL EKS CLUSTER MODULE (v21.4.0)
-#############################################
-
+# Terraform module to create Amazon Elastic Kubernetes
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.4.0"
-
   name               = var.cluster_name
   kubernetes_version = var.k8_version
 
   enable_irsa = true
 
-  ###########################################
-  # EKS ADDONS
-  ###########################################
   addons = {
     coredns = {
       most_recent = true
@@ -30,104 +23,72 @@ module "eks" {
       most_recent    = true
     }
   }
-
-  ###########################################
-  # CLUSTER ENDPOINT ACCESS
-  ###########################################
   endpoint_public_access       = true
   endpoint_private_access      = true
-  endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  endpoint_public_access_cidrs = ["0.0.0.0/0"] # NEED TO CHANGE THE VALUE OF PUBLIC IP IN HERE
 
-  ###########################################
-  # NETWORKING
-  ###########################################
+  # Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id                   = data.terraform_remote_state.global.outputs.vpc_id
   subnet_ids               = data.terraform_remote_state.global.outputs.private_subnet_ids
   control_plane_subnet_ids = data.terraform_remote_state.global.outputs.private_subnet_ids
 
-  enable_cluster_creator_admin_permissions = false
-
-
-  ###########################################
-  # ACCESS ENTRIES (IAM → Kubernetes RBAC)
-  # ONLY ADMIN USERS/ROLES GO HERE
-  ###########################################
-  access_entries = {
-    #########################################
-    # 1. LOCAL USER (ARJUN) → ADMIN
-    #########################################
-    admin_user = {
-      principal_arn     = "arn:aws:iam::677938781728:user/Arjun"
-      kubernetes_groups = ["system:masters"]
-      type              = "STANDARD"
-
-      policy_associations = {
-        admin = {
-          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = { type = "cluster" }
-        }
-      }
-    }
-
-    #########################################
-    # 2. GITHUB ACTIONS ROLE → ADMIN
-    #########################################
-    github_actions = {
-      principal_arn     = "arn:aws:iam::677938781728:role/github-actions-iam-role"
-      kubernetes_groups = ["system:masters"]
-      type              = "STANDARD"
-
-      policy_associations = {
-        admin = {
-          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = { type = "cluster" }
-        }
-      }
-    }
-  }
-
-  ###########################################
-  # EKS MANAGED NODE GROUP
-  ###########################################
+  # EKS Managed Node Group
   eks_managed_node_groups = {
-    workernode = {
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = [var.instance_type]
 
-      min_size       = var.min_count
-      max_size       = var.max_count
-      desired_size   = var.desired_count
+    # single workernode group with 1 desired EC2 instance
+    workernode = {
+      #depends_on = [module.eks.cluster_name]
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = [var.instance_type] # later change to t3.medium
+
+      min_size     = var.min_count
+      max_size     = var.max_count
+      desired_size = var.desired_count
     }
   }
 
-  ###########################################
-  # TAGS
-  ###########################################
   tags = {
     Environment = var.env
     Terraform   = "true"
   }
 }
 
-#############################################
-# WORKER NODE ACCESS ENTRY
-# MUST BE OUTSIDE MODULE TO AVOID CYCLES
-#############################################
-
-resource "aws_eks_access_entry" "node_access" {
+# Admin user
+resource "aws_eks_access_entry" "local_admin" {
   cluster_name  = module.eks.cluster_name
-
-  # Correct principal ARN for nodegroup
-  principal_arn = module.eks.eks_managed_node_groups["workernode"].iam_role_arn
-
-  type = "EC2_LINUX"
-
-  kubernetes_groups = [
-    "system:bootstrappers",
-    "system:nodes",
-  ]
-
-  depends_on = [
-    module.eks
-  ]
+  principal_arn = "arn:aws:iam::677938781728:user/Arjun"
+  type          = "STANDARD"
+  depends_on    = [module.eks]
 }
+
+resource "aws_eks_access_policy_association" "local_admin_cluster_admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_eks_access_entry.local_admin.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+# Github Action Role
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::677938781728:role/github-actions-iam-role"
+  type          = "STANDARD"
+  depends_on    = [module.eks]
+}
+
+resource "aws_eks_access_policy_association" "github_actions_admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_eks_access_entry.github_actions.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
