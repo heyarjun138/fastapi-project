71a35ff45f60dfd18858ab61e71e2322f6e9666c
Arjun <heyarjun138@gmail.com>
2025-11-18 12:03:22 +0530

    updated the versions.tf under loki-setup

---
+++ terraform/cluster/loki-setup/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+  required_version = ">= 1.6.0"
+}
