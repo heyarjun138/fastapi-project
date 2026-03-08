a7093129380e4a3db2256a874921cd0a0e163451
Arjun <heyarjun138@gmail.com>
2026-03-08 16:02:04 +0530

    updated

--- terraform/cluster/eks/eks.tf
+++ terraform/cluster/eks/eks.tf
@@ -70,13 +70,13 @@ module "eks" {
 }
 
 # Admin user
-/*
+
 resource "aws_eks_access_entry" "local_admin" {
   cluster_name  = module.eks.cluster_name
   principal_arn = "arn:aws:iam::677938781728:user/Arjun"
   type          = "STANDARD"
   depends_on    = [module.eks]
-}*/
+}
 
 resource "aws_eks_access_policy_association" "local_admin_cluster_admin" {
   cluster_name  = module.eks.cluster_name
